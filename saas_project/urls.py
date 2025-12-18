a7496c5db668f9829afe8aa769b342278ec40811
WarenOdhiambo1 <kisumuwebiner@gmail.com>
2025-12-19 00:42:02 +0300

    Add bi-directional Airtable sync

--- saas_project/urls.py
+++ saas_project/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('admin/users/', admin_user_management, name='admin_user_management'),
     path('admin/validate-email/', validate_email_ajax, name='validate_email_ajax'),
     path('admin/sync-users/', sync_users, name='sync_users'),
-    path('sync/from-airtable/', sync_from_airtable_view, name='sync_from_airtable'),
-    path('sync/to-airtable/', sync_to_airtable_view, name='sync_to_airtable'),
     path('api/v1/', include('core.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('core.urls')),
