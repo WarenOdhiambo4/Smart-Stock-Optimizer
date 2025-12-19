9056383b0eadf5b50b9d7a949bd66c080b167db8
WarenOdhiambo1 <kisumuwebiner@gmail.com>
2025-12-20 02:47:21 +0300

    Use environment variables for database config

--- saas_project/settings.py
+++ saas_project/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'saas_project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="postgres"),
-        "USER": config("DB_USER", default="postgres.bmbvkdzvdlgnncshpchw"),
-        "PASSWORD": config("DB_PASSWORD", default="xxyyz418712"),
-        "HOST": config("DB_HOST", default="aws-1-eu-west-2.pooler.supabase.com"),
-        "PORT": config("DB_PORT", default="5432"),
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+        "USER": os.environ.get("DB_USER", "postgres.bmbvkdzvdlgnncshpchw"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "xxyyz418712"),
+        "HOST": os.environ.get("DB_HOST", "aws-1-eu-west-2.pooler.supabase.com"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
         "OPTIONS": {
             "sslmode": "require",
         },
