fa8b8bdc3bcdf0b2a66b4e82aae3af7858c6ebd4
WarenOdhiambo1 <kisumuwebiner@gmail.com>
2026-04-17 18:13:12 +0300

    Ui content management page

--- core/pwa_views.py
+++ core/pwa_views.py
@@ -11,4 +11,8 @@ def manifest(request):
 
 def service_worker(request):
     sw_path = Path(settings.BASE_DIR) / "static" / "pwa" / "sw.js"
-    return HttpResponse(sw_path.read_text(encoding="utf-8"), content_type="application/javascript")
+    response = HttpResponse(sw_path.read_text(encoding="utf-8"), content_type="application/javascript")
+    response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = '0'
+    return response
